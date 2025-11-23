87da511b122959c5ee72d2e57ed726cb992d0031
GretaBr01 <gretabrognoli@gmail.com>
2025-11-23 20:37:05 +0100

    aggiunta logica bottone e led per accensione

--- main.c
+++ main.c
@@ -25,6 +25,89 @@
 #include "encoder_manager.h"
 #include "outsetting.h"
 
+/* External LEDs Red, Green and Blue: Port, Pin, Line */
+#define RLED_LINE   PAL_LINE( GPIOB, 10U ) // D6
+#define GLED_LINE   PAL_LINE( GPIOA, 8U ) // D4
+#define BLED_LINE   PAL_LINE( GPIOA, 9U ) // D5
+
+//#define BTN_ON_OFF_LINE PAL_LINE(GPIOC, 13U)
+#define BTN_ON_OFF_LINE PAL_LINE(GPIOA, 10U)
+
+#define ARRAY_LEN(a)            (sizeof(a)/sizeof(a[0]))
+
+volatile uint8_t flag_on_off = PAL_LOW;
+volatile uint8_t flag_error = PAL_LOW;
+volatile uint8_t funcionamiento = PAL_LOW;
+/*
+ * Shared Variable: time
+ */
+static uint32_t time;
+
+static void button_on_off_cb(void *arg) {
+
+  (void)arg;
+
+  static systime_t start, stop;
+  chSysLockFromISR();
+  if( palReadLine( BTN_ON_OFF_LINE ) == PAL_HIGH ) {
+    start = chVTGetSystemTimeX();
+  } else {
+    sysinterval_t delta;
+    stop = chVTGetSystemTimeX();
+    delta = chTimeDiffX( start, stop );
+    time = TIME_I2MS( delta );
+    if (time >= 150) flag_on_off = !flag_on_off;
+  }
+  chSysUnlockFromISR();
+}
+/*
+ * Working Area and Thread declarations.
+ */
+static THD_WORKING_AREA( waStatusLED, 128 );
+static THD_FUNCTION( thdStatusLED, arg ) {
+  (void) arg;
+
+  uint32_t i;
+  ioline_t leds[] = { RLED_LINE, GLED_LINE, BLED_LINE };
+
+  /* Setup Leds Outputs */
+  for( i = 0; i < ARRAY_LEN(leds); i++ ) {
+    palSetLineMode( leds[i], PAL_MODE_OUTPUT_PUSHPULL );
+  }
+
+  while( 1 ) {
+    if((flag_on_off == PAL_HIGH)&&(flag_error == PAL_LOW)) {
+      palClearLine(RLED_LINE);
+      palSetLine(GLED_LINE);
+      if(funcionamiento==PAL_LOW){
+        initOut();
+        startOut();
+        setShape(SIN);
+        startADCThread();
+        startOledThread();
+        startEncoderThread();
+        funcionamiento=PAL_HIGH;
+      }
+    }
+    else if((flag_on_off == PAL_LOW)&&(flag_error == PAL_LOW)){
+      palClearLine(GLED_LINE);
+      palSetLine(RLED_LINE);
+      if(funcionamiento==PAL_HIGH){
+        stopEncoderThread();
+        stopOledThread();
+        stopADCThread();
+        stopOut();
+
+        funcionamiento=PAL_LOW;
+      }
+    }
+    else {
+      if(palReadLine(GLED_LINE)==PAL_HIGH) palClearLine(GLED_LINE);
+      palToggleLine(RLED_LINE);
+    }
+    chThdSleepMilliseconds( 200 );
+  }
+}
 
 int main(void) {
 
@@ -39,14 +122,16 @@ int main(void) {
                  PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_HIGHEST |
                  PAL_STM32_PUPDR_PULLUP);
 
-  initOut();
-  startOut();
-  setShape(SIN);
+  /* Enabling events on both edges of the button line.*/
+  palEnableLineEvent( BTN_ON_OFF_LINE, PAL_EVENT_MODE_BOTH_EDGES);
+  palSetLineCallback( BTN_ON_OFF_LINE, button_on_off_cb, NULL);
+  chThdCreateStatic(waStatusLED, sizeof(waStatusLED), NORMALPRIO + 1, thdStatusLED, NULL );
 
+  palSetLineMode(LINE_LED,PAL_MODE_OUTPUT_PUSHPULL);
 
-  startADCThread();
-  startOledThread();
-  startEncoderThread();
+  palSetLineMode(BTN_ON_OFF_LINE, PAL_MODE_INPUT_PULLUP);
+  palEnableLineEvent(BTN_ON_OFF_LINE, PAL_EVENT_MODE_BOTH_EDGES);
+  palSetLineCallback(BTN_ON_OFF_LINE, button_on_off_cb, NULL);
 
   while (true ) {
     updateScope();
